6e99b8d7f0d455bcb0ca7d70ac5ba0d678e8ccaa
elisehammarstrom <32701757+elisehammarstrom@users.noreply.github.com>
2024-10-07 12:19:09 +0200

    tracked time of loading docs and changed instructions

--- app/app_gemini.py
+++ app/app_gemini.py
@@ -2,6 +2,7 @@ import google.generativeai as genai
 import os
 import streamlit as st
 from datetime import datetime
+import time
 from dotenv import load_dotenv
 import chromadb
 from chromadb import Documents, EmbeddingFunction, Embeddings
@@ -20,6 +21,8 @@ st.title("Ask1177")
 st.divider()
 st.caption("*Disclaimer:* This application was trained on data scraped from 1177.se. The chatbot should assist in getting health advice, but always remember, that it can not replace a doctor. It is a student project and not officially hosted by 1177.se.")
 st.divider()
+
+number_of_files = 509
 
 class GeminiEmbeddingFunction(EmbeddingFunction):
     def __call__(self, input: Documents) -> Embeddings:
@@ -41,6 +44,7 @@ def load_pdf(file_path):
 
 @st.cache_resource
 def create_chroma_db():
+    start_time = time.time()
     chroma_client = chromadb.PersistentClient(path="contents")
 
     existing_collections = chroma_client.list_collections()
@@ -52,14 +56,9 @@ def create_chroma_db():
         chroma_client.delete_collection(name=name)
         print("------- DELETED DATABASE -------")
 
-
-    # if "rag_experiment" in chroma_client.list_collections():
-    #     return chroma_client.get_collection("rag_experiment")
-    
-    
     db = chroma_client.create_collection(name="rag_experiment", embedding_function=GeminiEmbeddingFunction())
 
-    for i in range(50):  # Adjust range as needed
+    for i in range(number_of_files):  # Adjust range as needed
         file_path = f"../scraping/pdf_downloads/child_page_{i+1}.pdf"
         if os.path.exists(file_path):
             text = load_pdf(file_path)
@@ -68,7 +67,12 @@ def create_chroma_db():
             else:
                 db.add(documents=[text], ids=[str(i)])
                 print(f"documents{i+1} was loaded")
-            #db.add(documents=[text], ids=[str(i)])
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{timestamp} : All documents loaded. Total time: {elapsed_time:.2f} seconds")
+    
     return db
 
 # Create or load the ChromaDB
@@ -83,9 +87,11 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # Input prompt from user
-timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(f"{timestamp} : Before message compute")    
 if prompt := st.chat_input("What symptoms do you have?"):
+    start_time = time.time()
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{timestamp} : User asked a question")
+
     st.session_state.messages.append({"role": "user", "content": prompt})
     
     with st.chat_message("user"):
@@ -97,7 +103,7 @@ if prompt := st.chat_input("What symptoms do you have?"):
 
     # Generate response using Google Gemini
     response = model.generate_content([
-        "You are a knowledgeable resource providing general information about medical conditions based on the content from the uploaded files. Explain concepts thoroughly while ensuring clarity for a non-technical audience. When symptoms are provided, please offer a potential diagnosis. Always emphasize that users should consult a healthcare professional for personalized medical advice.",
+        "You are a knowledgeable resource providing general information about medical conditions based primarily on the content in the context. Explain concepts thoroughly while ensuring clarity for a non-technical audience. When symptoms are provided, please offer a potential diagnosis. Always emphasize that users should consult a healthcare professional for personalized medical advice.",
         f"Context: {context}",
         f"User question: {prompt}"
     ])
@@ -107,3 +113,8 @@ if prompt := st.chat_input("What symptoms do you have?"):
     
     # Append the assistant's response to the messages
     st.session_state.messages.append({"role": "assistant", "content": response.text})
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{timestamp} : Response generated. Total time: {elapsed_time:.2f} seconds")
